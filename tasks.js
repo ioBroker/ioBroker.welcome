a024989f34fbb3e183ec3150f712ae304e999986
GermanBluefox <dogafox@gmail.com>
2024-10-03 12:43:33 +0800

    Debug CI

--- tasks.js
+++ tasks.js
@@ -1,4 +1,5 @@
 const { deleteFoldersRecursive, buildReact, npmInstall, copyFiles } = require('@iobroker/build-tools');
+const { existsSync } = require('node:fs');
 
 function adminCopyAllFiles() {
     copyFiles(['src-admin/build/static/js/*.js', '!src-admin/build/static/js/vendors*.js'], 'admin/custom/static/js');
@@ -21,22 +22,40 @@ if (process.argv.includes('--0-admin-clean')) {
 } else if (process.argv.includes('--1-admin-npm')) {
     npmInstall(`${__dirname}/src-admin`).catch(e => console.error(`Cannot install npm: ${e}`));
 } else if (process.argv.includes('--2-admin-build')) {
-    buildReact(`${__dirname}/src-admin`, { rootDir: __dirname, craco: true }).catch(e =>
-        console.error(`Cannot build: ${e}`),
-    );
+    buildReact(`${__dirname}/src-admin`, { rootDir: __dirname, craco: true })
+        .then(() => {
+            if (!existsSync(`${__dirname}/src-admin/build/index.html`)) {
+                console.error(`Cannot find ${__dirname}/src-admin/build/index.html after build`);
+                process.exit(1);
+            }
+        })
+        .catch(e => console.error(`Cannot build: ${e}`));
 } else if (process.argv.includes('--3-admin-copy')) {
     adminCopyAllFiles();
 } else if (process.argv.includes('--admin')) {
     adminClean();
     npmInstall(`${__dirname}/src-admin`)
         .then(() => buildReact(`${__dirname}/src-admin`, { rootDir: __dirname, craco: true }))
+        .then(() => {
+            if (!existsSync(`${__dirname}/src-admin/build/index.html`)) {
+                console.error(`Cannot find ${__dirname}/src-admin/build/index.html after build`);
+                process.exit(1);
+            }
+        })
         .then(() => adminCopyAllFiles());
 } else if (process.argv.includes('--0-clean')) {
     deleteFoldersRecursive('public');
 } else if (process.argv.includes('--1-npm')) {
     npmInstall(`${__dirname}/src`).catch(e => console.error(`Cannot install npm: ${e}`));
 } else if (process.argv.includes('--2-build')) {
-    buildReact(`${__dirname}/src`, { rootDir: __dirname }).catch(e => console.error(`Cannot build: ${e}`));
+    buildReact(`${__dirname}/src`, { rootDir: __dirname })
+        .then(() => {
+            if (!existsSync(`${__dirname}/src/build/index.html`)) {
+                console.error(`Cannot find ${__dirname}/src/build/index.html after build`);
+                process.exit(1);
+            }
+        })
+        .catch(e => console.error(`Cannot build: ${e}`));
 } else if (process.argv.includes('--3-copy')) {
     copyFiles(
         ['src/build/*/**', 'src/build/*', '!src/build/static/media/*.svg', '!src/build/static/media/*.txt'],
@@ -46,6 +65,12 @@ if (process.argv.includes('--0-admin-clean')) {
     deleteFoldersRecursive('public');
     npmInstall(`${__dirname}/src`)
         .then(() => buildReact(`${__dirname}/src`, { rootDir: __dirname }))
+        .then(() => {
+            if (!existsSync(`${__dirname}/src/build/index.html`)) {
+                console.error(`Cannot find ${__dirname}/src/build/index.html after build`);
+                process.exit(1);
+            }
+        })
         .then(() =>
             copyFiles(
                 ['src/build/*/**', 'src/build/*', '!src/build/static/media/*.svg', '!src/build/static/media/*.txt'],
@@ -58,9 +83,21 @@ if (process.argv.includes('--0-admin-clean')) {
     deleteFoldersRecursive('public');
     npmInstall(`${__dirname}/src-admin`)
         .then(() => buildReact(`${__dirname}/src-admin`, { rootDir: __dirname, craco: true }))
+        .then(() => {
+            if (!existsSync(`${__dirname}/src-admin/build/index.html`)) {
+                console.error(`Cannot find ${__dirname}/src-admin/build/index.html after build`);
+                process.exit(1);
+            }
+        })
         .then(() => adminCopyAllFiles())
         .then(() => npmInstall(`${__dirname}/src`))
         .then(() => buildReact(`${__dirname}/src`, { rootDir: __dirname }))
+        .then(() => {
+            if (!existsSync(`${__dirname}/src/build/index.html`)) {
+                console.error(`Cannot find ${__dirname}/src/build/index.html after build`);
+                process.exit(1);
+            }
+        })
         .then(() =>
             copyFiles(
                 ['src/build/*/**', 'src/build/*', '!src/build/static/media/*.svg', '!src/build/static/media/*.txt'],
